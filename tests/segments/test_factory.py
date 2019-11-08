e4643d4bb8d1fe17fff2713ffcfb6468bc3e8adb
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2019-11-08 20:41:23 +0100

    Add tests for segment factory

---
+++ tests/segments/test_factory.py
@@ -0,0 +1,45 @@
+import unittest
+
+from pinch import segments
+from pinch.segments.latent_segment import LatentSegment
+from pinch.segments.sensible_segment import SensibleSegment
+
+
+class TestSegmentFactory(unittest.TestCase):
+    """
+    Test class for segment factory
+    """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_latent_segment(self):
+        self.assertEqual(
+            segments.create(200, 100, 100), LatentSegment(200, 100, None))
+        self.assertEqual(
+            segments.create(-150, 80, 80), LatentSegment(-150, 80, None))
+
+    def test_create_latent_segment_with_temp_diff_contrib(self):
+        self.assertEqual(
+            segments.create(200, 100, 100, 10), LatentSegment(200, 100, 10))
+        self.assertEqual(
+            segments.create(-150, 80, 80, 10), LatentSegment(-150, 80, 10))
+
+    def test_create_sensible_segment(self):
+        self.assertEqual(
+            segments.create(150, 20, 80), SensibleSegment(2.5, 20, 80, None))
+        self.assertEqual(
+            segments.create(-150, 100, 50), SensibleSegment(3, 100, 50, None))
+
+    def test_create_sensible_segment_with_temp_diff_contrib(self):
+        self.assertEqual(
+            segments.create(150, 20, 80, 5), SensibleSegment(2.5, 20, 80, 5))
+        self.assertEqual(
+            segments.create(-150, 100, 50, 5), SensibleSegment(3, 100, 50, 5))
+
+
+if __name__ == "__main__":
+    unittest.main()

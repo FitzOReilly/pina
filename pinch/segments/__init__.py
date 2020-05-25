dd4984373ebf7d33a6c97c9634152bf1510f5247
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2020-05-25 02:12:05 +0200

    Rename segment back to segments

--- pinch/segments/__init__.py
+++ pinch/segments/__init__.py
@@ -3,10 +3,10 @@ def new(heat_flow, supply_temp, target_temp, temp_shift=None):
     Creates a new segment with the given properties.
     """
     if supply_temp == target_temp:
-        from pinch.segment.latent_segment import LatentSegment
+        from pinch.segments.latent_segment import LatentSegment
         cls = LatentSegment
     else:
-        from pinch.segment.sensible_segment import SensibleSegment
+        from pinch.segments.sensible_segment import SensibleSegment
         cls = SensibleSegment
 
     return cls.new(heat_flow, supply_temp, target_temp, temp_shift)

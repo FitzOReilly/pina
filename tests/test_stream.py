f8d114bfddcdcac0537b451d74fa7c4b4f0247c9
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2020-07-29 00:22:06 +0200

    Use argument list instead of list in Stream.__init__

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -25,32 +25,32 @@ class TestStream(unittest.TestCase):
 
         self.mixed_segments = [make_segment(-320, 20, 100), make_segment(320, 100, 20)]
 
-        self.single_sensible_segment = Stream(self.cold_segments[:1])
-        self.single_latent_segment = Stream(self.hot_segments[1:2])
-        self.cold_stream = Stream(self.cold_segments)
-        self.hot_stream = Stream(self.hot_segments)
-        self.neutral_stream = Stream(self.mixed_segments)
+        self.single_sensible_segment = Stream(*self.cold_segments[:1])
+        self.single_latent_segment = Stream(*self.hot_segments[1:2])
+        self.cold_stream = Stream(*self.cold_segments)
+        self.hot_stream = Stream(*self.hot_segments)
+        self.neutral_stream = Stream(*self.mixed_segments)
 
     def tearDown(self):
         pass
 
     def test_segments_empty(self):
         with self.assertRaises(ValueError):
-            Stream([])
+            Stream()
 
     def test_temperature_mismatch(self):
         mismatch_first = [make_segment(-150, 50, 200), *self.cold_segments]
         with self.assertRaises(ValueError):
-            Stream(mismatch_first)
+            Stream(*mismatch_first)
 
         mismatch_middle = self.cold_segments
         mismatch_middle[1] = make_segment(-120, 400, 400)
         with self.assertRaises(ValueError):
-            Stream(mismatch_middle)
+            Stream(*mismatch_middle)
 
         mismatch_last = [*self.cold_segments, make_segment(-150, 50, 200)]
         with self.assertRaises(ValueError):
-            Stream(mismatch_last)
+            Stream(*mismatch_last)
 
     def test_supply_temp(self):
         self.assertEqual(self.single_sensible_segment.supply_temp, 20)
@@ -90,7 +90,7 @@ class TestStream(unittest.TestCase):
             make_segment(0, 0, -20),
         ]
 
-        stream = Stream(test_segments)
+        stream = Stream(*test_segments)
 
         self.assertEqual(stream.segments, test_segments)
         self.assertEqual(
@@ -106,24 +106,22 @@ class TestStream(unittest.TestCase):
         )
 
     def test_make_stream(self):
-        from_init = Stream([make_segment(-320, 20, 100, 5)])
+        from_init = Stream(make_segment(-320, 20, 100, 5))
         from_make = make_stream(-320, 20, 100, 5)
         self.assertEqual(from_init.segments, from_make.segments)
 
-        from_init = Stream([make_segment(-400, 100, 100)])
+        from_init = Stream(make_segment(-400, 100, 100))
         from_make = make_stream(-400, 100, 100)
         self.assertEqual(from_init.segments, from_make.segments)
 
     def test_make_segmented_stream(self):
         from_init = Stream(
-            [
-                make_segment(-320, 20, 100, 5),
-                make_segment(-400, 100, 100),
-                make_segment(0, 100, 100, 10),
-                make_segment(400, 100, 0),
-                make_segment(300, 0, 0),
-                make_segment(0, 0, -20),
-            ]
+            make_segment(-320, 20, 100, 5),
+            make_segment(-400, 100, 100),
+            make_segment(0, 100, 100, 10),
+            make_segment(400, 100, 0),
+            make_segment(300, 0, 0),
+            make_segment(0, 0, -20),
         )
 
         from_make = make_segmented_stream(

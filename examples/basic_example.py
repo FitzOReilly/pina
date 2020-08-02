aaa2e227382c535c5b9c5967a31676f6157b86c0
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2020-08-02 19:31:38 +0200

    Write Readme

---
+++ examples/basic_example.py
@@ -0,0 +1,55 @@
+from pina import PinchAnalyzer, make_stream
+
+# Arguments: heat flow, supply temperature, target temperature
+cold_1 = make_stream(-230, 20, 135)
+hot_1 = make_stream(330, 170, 60)
+cold_2 = make_stream(-240, 80, 140)
+hot_2 = make_stream(180, 150, 30)
+
+min_temp_diff = 10
+temp_shift = min_temp_diff / 2
+
+analyzer = PinchAnalyzer(temp_shift)
+analyzer.add_streams(cold_1, hot_1, cold_2, hot_2)
+
+print(
+    "Heating demand:       {}\n"
+    "Cooling demand:       {}\n"
+    "Hot utility target:   {}\n"
+    "Cold utility target:  {}\n"
+    "Heat recovery target: {}\n"
+    "Pinch temperature(s): {}".format(
+        analyzer.heating_demand,
+        analyzer.cooling_demand,
+        analyzer.hot_utility_target,
+        analyzer.cold_utility_target,
+        analyzer.heat_recovery_target,
+        analyzer.pinch_temps,
+    )
+)
+
+
+from matplotlib import pyplot as plt
+
+fig, ax = plt.subplots(1, 2, figsize=(16, 7))
+
+ax[0].plot(*analyzer.hot_composite_curve, color="tab:red", linestyle="--", label="HCC")
+ax[0].plot(*analyzer.cold_composite_curve, color="tab:blue", linestyle="-", label="CCC")
+ax[0].legend()
+ax[0].set_title("Hot and cold composite curves")
+ax[0].set_xlabel("Heat flow [kW]")
+ax[0].set_ylabel("Actual temperature [\u2103]")
+
+ax[1].plot(*analyzer.grand_composite_curve, color="k", linestyle="-", label="GCC")
+ax[1].legend()
+ax[1].set_title("Grand composite curve")
+ax[1].set_xlabel("Net heat flow [kW]")
+ax[1].set_ylabel("Shifted temperature [\u2103]")
+
+# Make the y-axis equal in both plots
+ylims = (*ax[0].get_ylim(), *ax[1].get_ylim())
+minmax_ylims = (min(ylims), max(ylims))
+ax[0].set_ylim(minmax_ylims)
+ax[1].set_ylim(minmax_ylims)
+
+plt.show()

f5309323817416bc172982bf500c3fca136e115e
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2020-10-12 02:09:15 +0200

    Add package docstring and version

--- src/pina/__init__.py
+++ src/pina/__init__.py
@@ -1,4 +1,8 @@
+"""A lightweight pinch analysis package"""
+
 from pina.pinch_analyzer import PinchAnalyzer
 from pina.stream import make_segmented_stream, make_stream
+
+__version__ = "0.1.0"
 
 __all__ = ["PinchAnalyzer", "make_segmented_stream", "make_stream"]
